7c3a5131422ab5fc5c5acaf881245d770eda0d4e
KevinLi <shawnli1104@gmail.com>
2019-04-04 18:57:27 +0800

    添加Android cybex签名库

--- android/src/main/java/com/nbltrustdev/cybex/cybex_flutter_plugin/CybexFlutterPlugin.java
+++ android/src/main/java/com/nbltrustdev/cybex/cybex_flutter_plugin/CybexFlutterPlugin.java
@@ -1,5 +1,9 @@
 package com.nbltrustdev.cybex.cybex_flutter_plugin;
 
+import com.cybex.provider.graphene.chain.PrivateKey;
+import com.cybex.provider.graphene.chain.SignedTransaction;
+import com.cybex.provider.graphene.chain.Types;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -17,7 +21,10 @@ public class CybexFlutterPlugin implements MethodCallHandler {
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+      PrivateKey privateKey = PrivateKey.from_seed("cybex-test" + "active" + "cybextest123456");
+      Types.public_key_type publicActiveKeyType = new Types.public_key_type(privateKey.get_public_key(true), true);
+
+      result.success("Android " + publicActiveKeyType.toString());
     } else {
       result.notImplemented();
     }
